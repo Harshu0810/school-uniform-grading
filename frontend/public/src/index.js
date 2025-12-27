26dc8c792f87d9b9df0ccecce228f817e301848a
Harshu0810 <143718146+Harshu0810@users.noreply.github.com>
2025-12-27 18:53:34 +0530

    Update index.js

--- frontend/public/src/index.js
+++ frontend/public/src/index.js
@@ -1,1 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './styles/globals.css';
+import App from './App';
 
+// Create root React container
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+// Render app
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
