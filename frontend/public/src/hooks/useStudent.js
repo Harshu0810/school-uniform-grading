da630c8266c35cd4ec9434d85013b999f0ac0228
Harshu0810 <143718146+Harshu0810@users.noreply.github.com>
2025-12-27 18:38:48 +0530

    Update useStudent.js

--- frontend/public/src/hooks/useStudent.js
+++ frontend/public/src/hooks/useStudent.js
@@ -1,1 +1,14 @@
 
+import { useState, useEffect } from 'react';
+import { useAuth } from '../context/AuthContext';
+
+export default function useStudent() {
+  const { userData } = useAuth();
+  const [studentData, setStudentData] = useState(null);
+
+  useEffect(() => {
+    setStudentData(userData);
+  }, [userData]);
+
+  return studentData;
+}
